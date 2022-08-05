2dea849c74f03329f02763cf14821683b43915df
yuangezhizao <root@yuangezhizao.cn>
2022-08-05 23:23:16 +0800

    ✨ Feat(e53_sc1_light.c): 新增 e53_sc1_light 示例

---
+++ examples/e53_sc1_light.c
@@ -0,0 +1,23 @@
+#include "tos_k.h"
+
+void init_e53_sc1_light(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+}
+
+void application_entry(void *arg)
+{
+    init_e53_sc1_light();
+    while (1)
+    {
+        GPIO_SetBits(GPIOC, GPIO_Pin_10);
+        Delay_Ms(1000);
+        GPIO_ResetBits(GPIOC, GPIO_Pin_10);
+        Delay_Ms(1000);
+    }
+}

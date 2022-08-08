5057759022ca3ae8bc1808f25719fb760d873416
yuangezhizao <root@yuangezhizao.cn>
2022-08-08 20:27:41 +0800

    ✨ Feat(gpio_toggle.c): 新增 gpio_toggle + exti0 示例

--- User/ch32v30x_it.c
+++ User/ch32v30x_it.c
@@ -1,48 +1,48 @@
 /********************************** (C) COPYRIGHT *******************************
-* File Name          : ch32v30x_it.c
-* Author             : WCH
-* Version            : V1.0.0
-* Date               : 2021/06/06
-* Description        : Main Interrupt Service Routines.
-*******************************************************************************/
+ * File Name          : ch32v30x_it.c
+ * Author             : WCH
+ * Version            : V1.0.0
+ * Date               : 2021/06/06
+ * Description        : Main Interrupt Service Routines.
+ *******************************************************************************/
 #include "ch32v30x_it.h"
 #include "tos_k.h"
 #include "tos_at.h"
 extern at_agent_t esp8266_tf_agent;
-//extern at_agent_t esp8266_agent;
+// extern at_agent_t esp8266_agent;
 
 void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
 void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
 void USART2_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
 void UART6_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
 void UART7_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void EXTI0_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
 
 /*******************************************************************************
-* Function Name  : NMI_Handler
-* Description    : This function handles NMI exception.
-* Input          : None
-* Return         : None
-*******************************************************************************/
+ * Function Name  : NMI_Handler
+ * Description    : This function handles NMI exception.
+ * Input          : None
+ * Return         : None
+ *******************************************************************************/
 void NMI_Handler(void)
 {
 }
 
 /*******************************************************************************
-* Function Name  : HardFault_Handler
-* Description    : This function handles Hard Fault exception.
-* Input          : None
-* Return         : None
-*******************************************************************************/
+ * Function Name  : HardFault_Handler
+ * Description    : This function handles Hard Fault exception.
+ * Input          : None
+ * Return         : None
+ *******************************************************************************/
 void HardFault_Handler(void)
 {
-    printf("hardfault\r\n");
-    printf("mepc   = %08x\r\n",__get_MEPC());
-    printf("mcause = %08x\r\n",__get_MCAUSE());
-    printf("mtval  = %08x\r\n",__get_MTVAL());
-    while (1)
-    {
-
-    }
+  printf("hardfault\r\n");
+  printf("mepc   = %08x\r\n", __get_MEPC());
+  printf("mcause = %08x\r\n", __get_MCAUSE());
+  printf("mtval  = %08x\r\n", __get_MTVAL());
+  while (1)
+  {
+  }
 }
 
 /*********************************************************************
@@ -55,32 +55,28 @@ void HardFault_Handler(void)
 void USART2_IRQHandler(void)
 {
   uint8_t data;
-  if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
+  if (USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
   {
-      data= USART_ReceiveData(USART2);
-      tos_at_uart_input_byte(&esp8266_tf_agent,data);
+    data = USART_ReceiveData(USART2);
+    tos_at_uart_input_byte(&esp8266_tf_agent, data);
   }
-
 }
 
-
 /*********************************************************************
-* @fn      USART2_IRQHandler
-*
-* @brief   This function handles USART2 global interrupt request.
-*
-* @return  none
-*/
+ * @fn      USART2_IRQHandler
+ *
+ * @brief   This function handles USART2 global interrupt request.
+ *
+ * @return  none
+ */
 void UART6_IRQHandler(void)
 {
-   uint8_t data;
- if(USART_GetITStatus(UART6, USART_IT_RXNE) != RESET)
- {
-     data= USART_ReceiveData(UART6);
-     tos_at_uart_input_byte(&esp8266_tf_agent,data);
-
- }
-
+  uint8_t data;
+  if (USART_GetITStatus(UART6, USART_IT_RXNE) != RESET)
+  {
+    data = USART_ReceiveData(UART6);
+    tos_at_uart_input_byte(&esp8266_tf_agent, data);
+  }
 }
 /*********************************************************************
  * @fn      USART2_IRQHandler
@@ -91,11 +87,33 @@ void UART6_IRQHandler(void)
  */
 void UART7_IRQHandler(void)
 {
-    uint8_t data;
-  if(USART_GetITStatus(UART7, USART_IT_RXNE) != RESET)
+  uint8_t data;
+  if (USART_GetITStatus(UART7, USART_IT_RXNE) != RESET)
   {
-      data= USART_ReceiveData(UART7);
-      tos_at_uart_input_byte(&esp8266_tf_agent,data);
+    data = USART_ReceiveData(UART7);
+    tos_at_uart_input_byte(&esp8266_tf_agent, data);
   }
+}
 
+/*********************************************************************
+ * @fn      EXTI0_IRQHandler
+ *
+ * @brief   This function handles EXTI0 Handler.
+ *
+ * @return  none
+ */
+
+volatile uint16_t User_LED_1_Status = 0;
+void EXTI0_IRQHandler(void)
+{
+  if (EXTI_GetITStatus(EXTI_Line0) != RESET)
+  {
+#if 0
+    printf("Run at EXTI\r\n");
+
+#endif
+    EXTI_ClearITPendingBit(EXTI_Line0); /* Clear Flag */
+    User_LED_1_Status = !User_LED_1_Status;
+    GPIO_WriteBit(GPIOE, GPIO_Pin_2, User_LED_1_Status);
+  }
 }
---
+++ examples/gpio_toggle.c
@@ -0,0 +1,60 @@
+/********************************** (C) COPYRIGHT *******************************
+ * File Name          : main.c
+ * Author             : WCH
+ * Version            : V1.0.0
+ * Date               : 2021/06/06
+ * Description        : Main program body.
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * SPDX-License-Identifier: Apache-2.0
+ *******************************************************************************/
+
+/*
+ *@Note
+ GPIO例程：
+ PE2推挽输出。
+*/
+
+//#include "debug.h"
+#include "tos_k.h"
+
+/* Global define */
+
+/* Global Variable */
+
+/*********************************************************************
+ * @fn      GPIO_Toggle_INIT
+ *
+ * @brief   Initializes GPIOA.0
+ *
+ * @return  none
+ */
+void GPIO_Toggle_INIT(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOE, &GPIO_InitStructure);
+}
+
+void application_entry(void *arg)
+{
+    //    u8 i = 0;
+
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+    //    Delay_Init();
+    //    USART_Printf_Init(115200);
+    printf("SystemClk:%d\r\n", SystemCoreClock);
+
+    printf("GPIO Toggle TEST\r\n");
+    EXTI0_INT_INIT();
+    GPIO_Toggle_INIT();
+
+    while (1)
+    {
+        //        Delay_Ms(250);
+        //        GPIO_WriteBit(GPIOA, GPIO_Pin_2, (i == 0) ? (i = Bit_SET) : (i = Bit_RESET));
+    }
+}
